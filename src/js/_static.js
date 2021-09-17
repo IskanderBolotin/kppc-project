2f347883c2002134e8c28254bb9a3a6dbbb7ef92
Alexander Bolotin <sanya.bolotin.96@mail.ru>
2021-09-17 17:54:02 +0300

    новые стринцы

--- src/js/_static.js
+++ src/js/_static.js
@@ -10,8 +10,10 @@ $(document).ready(function(){
         }
     })
     $("body").on("click", "[data-side-open]", function(){
-        $(this).parents("[data-side-menu]").find("[data-side-fixed]").addClass("__active");
-        $("[data-overlay-static]").addClass("__active");
+        if ($(window).outerWidth() < 1150) {
+            $(this).parents("[data-side-menu]").find("[data-side-fixed]").addClass("__active");
+            $("[data-overlay-static]").addClass("__active");
+        }
     })
     $("body").on("click", "[data-side-cls]", function(){
         $(this).parents("[data-side-fixed]").removeClass("__active");
@@ -29,4 +31,47 @@ $(document).ready(function(){
 			}
 		}
 	});
+    $(".gallerySlides").lightGallery({
+		selector: ".gallery-item",
+	})
+    let adaptStatic = false;
+    if ($(window).outerWidth() < 1151) {
+        if (!adaptStatic) {
+            $(".staticExtra").appendTo(".staticExtra-mb");
+            adaptStatic = true
+        }
+    }
+    $(window).on("resize", function(){
+        if ($(window).outerWidth() < 1151) {
+            if (!adaptStatic) {
+                $(".staticExtra").appendTo(".staticExtra-mb");
+                adaptStatic = true
+            }
+        }
+        else {
+            if (adaptStatic) {
+                $(".staticExtra").appendTo(".staticExtra-dt");
+                adaptStatic = false
+            }
+        }
+    });
+    $("body").on("click", ".staticLink[href^='#']", function(e){
+        e.preventDefault();
+        let _this = $(this)
+        let offset_top = $("#main-header").outerHeight();
+        $(this).parents("[data-side-fixed]").find(".staticLink").removeClass("__active");
+        $(this).addClass("__active");
+        $(this).parents("[data-side-fixed]").removeClass("__active");
+        $("[data-overlay-static]").removeClass("__active");
+        $(this).parents("[data-side-fixed]").on("transitionend", scrollToTag)
+        function scrollToTag() {
+            $("html, body").animate({
+                scrollTop: +$(_this.attr("href")).offset().top - +offset_top + "px"
+            }, {
+                duration: 500,
+            });
+            _this.parents("[data-side-fixed]").off("transitionend", scrollToTag)
+        }
+        
+    })
 });
